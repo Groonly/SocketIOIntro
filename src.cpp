19fec58cf49b31c85f94cb8ea2b66e3baf4ed3cc
Joel Martinsson <Joel.martinsson.molk@gmail.com>
2017-12-19 09:42:44 +0100

    added switch case in src

--- src.cpp
+++ src.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 int main(int argc, char *argv[]){
-  cout << argv[1] << endl;
-  cout << "4";
+  int a = atoi(argv[1]);
+  switch(a){
+    case 1:
+      cout << "10" << endl;
+      break;
+    case 2:
+      cout << "20" << endl;
+      break;
+    default:
+      cout << "0" << endl;
+      break;
+  }
 }
